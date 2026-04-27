b62527832162f419679c1dd53aee4d31e97dc7ce
mooner1213 <mooner948@gmail.com>
2026-04-27 16:16:13 +0900

    1. Hello, World 테스트 및 과제 작성

---
+++ Assets/Scripts/HelloUnity/UsingDemo.cs
@@ -0,0 +1,16 @@
+// using UnityEngine을 사용하지 않는다.
+
+public class UsingDemo : UnityEngine.MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UnityEngine.Debug.Log("Hello,Unity!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
